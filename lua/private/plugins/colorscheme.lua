5efdef732e6a0f4e259f543d333ed514c3b9e654
Sant Raj <santraj611@gmail.com>
2024-03-24 20:45:41 +0530

    changed colorscheme to gruvbox

--- lua/private/plugins/colorscheme.lua
+++ lua/private/plugins/colorscheme.lua
@@ -1,9 +1,6 @@
-return { 
-	"rose-pine/neovim",
-	name = "rose-pine",
-	lazy = false,
-	priority = 1000,
-	config = function()
-        vim.cmd.colorscheme("rose-pine")
-	end
+return {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000 ,
+    config = function() vim.cmd.colorscheme("gruvbox")
+    end
 }

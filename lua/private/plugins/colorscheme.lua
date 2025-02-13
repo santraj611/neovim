24dfe530aeb395e54991713477a13f8ea7d319e7
Sant Raj <santraj611@gmail.com>
2025-02-13 21:23:30 +0530

    removed tranparency

--- lua/private/plugins/colorscheme.lua
+++ lua/private/plugins/colorscheme.lua
@@ -7,8 +7,8 @@ return {
         vim.opt.background = "dark" -- set this to dark or light
         vim.cmd("colorscheme oxocarbon")
 
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+        -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
     end,
 }

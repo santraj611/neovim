79e1ebe1cfa39f2bbb18d5ca9ba60c7b19e88c26
Sant Raj <santraj611@gmail.com>
2025-03-12 22:24:05 +0530

    switched to catp theme

--- lua/private/plugins/colorscheme.lua
+++ lua/private/plugins/colorscheme.lua
@@ -1,14 +1,47 @@
 return {
-    "nyoom-engineering/oxocarbon.nvim",
+    "catppuccin/nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
-    config = function ()
-        vim.opt.background = "dark" -- set this to dark or light
-        vim.cmd("colorscheme oxocarbon")
+        no_italic = true,
+        term_colors = true,
+        transparent_background = false,
 
-        -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    config = function ()
+        local catppuccin = require("catppuccin");
+        catppuccin.setup({
+            styles = {
+                comments = { "italic" }, -- Change the style of comments
+                conditionals = {},
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+            },
+            color_overrides = {
+                mocha = {
+                    base = "#000000",
+                    mantle = "#000000",
+                    crust = "#000000",
+                },
+            },
+            integrations = {
+                telescope = {
+                    enabled = true,
+                    style = "nvchad",
+                },
+                dropbar = {
+                    enabled = true,
+                    color_mode = true,
+                },
+            },
+
+        })
+
+        catppuccin.load();
     end,
 }

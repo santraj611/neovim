4ba1cba0981786eaeb9ea3ac7b8d24878dda34b7
Sant Raj <santraj611@gmail.com>
2025-03-12 22:23:28 +0530

    added icons in lsp

--- lua/private/plugins/nvim-cmp.lua
+++ lua/private/plugins/nvim-cmp.lua
@@ -9,14 +9,33 @@ return {
         -- For luasnip users.
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
-        "rafamadriz/friendly-snippets",
+        --For Dev Icons
+        "nvim-tree/nvim-web-devicons",
+        "onsails/lspkind.nvim"
     },
+
     config = function()
         local cmp = require("cmp")
-
         require("luasnip.loaders.from_vscode").lazy_load()
-
         cmp.setup({
+            window = {
+                completion = {
+                    winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+                    col_offset = -3,
+                    side_padding = 0,
+                },
+            },
+            formatting = {
+                fields = { "kind", "abbr", "menu" },
+                format = function(entry, vim_item)
+                    local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+                    local strings = vim.split(kind.kind, "%s", { trimempty = true })
+                    kind.kind = " " .. (strings[1] or "") .. " "
+                    kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+                    return kind
+                end,
+            },
             completion = {
                 completeopt = "menu, menuone, preview, noselect",
             },

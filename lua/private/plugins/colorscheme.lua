8e7d5e2905c7cce63ff175608b77565d4fb08530
Sant Raj <santraj611@gmail.com>
2025-03-31 22:22:21 +0530

    switched my my own custom plugin

--- lua/private/plugins/colorscheme.lua
+++ lua/private/plugins/colorscheme.lua
@@ -1,47 +1,8 @@
 return {
-    "catppuccin/nvim",
-    lazy = false,
-    priority = 1000,
-        no_italic = true,
-        term_colors = true,
-        transparent_background = false,
-
+    "santraj611/custom-onedark",
+    lazy = false, -- Load on startup OR load when :colorscheme is called
+    priority = 1000, -- Make sure it loads before other plugins that depend on colors
     config = function ()
-        local catppuccin = require("catppuccin");
-        catppuccin.setup({
-            styles = {
-                comments = { "italic" }, -- Change the style of comments
-                conditionals = {},
-                loops = {},
-                functions = {},
-                keywords = {},
-                strings = {},
-                variables = {},
-                numbers = {},
-                booleans = {},
-                properties = {},
-                types = {},
-            },
-            color_overrides = {
-                mocha = {
-                    base = "#000000",
-                    mantle = "#000000",
-                    crust = "#000000",
-                },
-            },
-            integrations = {
-                telescope = {
-                    enabled = true,
-                    style = "nvchad",
-                },
-                dropbar = {
-                    enabled = true,
-                    color_mode = true,
-                },
-            },
-
-        })
-
-        catppuccin.load();
-    end,
+        vim.cmd("colorscheme custom-onedark")
+    end
 }

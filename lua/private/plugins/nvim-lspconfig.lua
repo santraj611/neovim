57d3309ae716a2aba4f97d6e1e7f239495a6c80f
Sant Raj <santraj611@gmail.com>
2025-05-13 13:46:42 +0530

    c lsp readded

--- lua/private/plugins/nvim-lspconfig.lua
+++ lua/private/plugins/nvim-lspconfig.lua
@@ -58,6 +58,12 @@ return {
             on_attach = on_attach,
         })
 
+        -- configure c/c++ server
+        lspconfig["clangd"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+        })
+
         -- configure lua server (with special settings)
         lspconfig["lua_ls"].setup({
             capabilities = capabilities,

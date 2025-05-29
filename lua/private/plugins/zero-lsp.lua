230f101bc177cb7883d5f5ec046446bc64bc720f
Sant Raj <santraj611@gmail.com>
2025-05-29 22:32:39 +0530

    switched to lsp-zero

---
+++ lua/private/plugins/zero-lsp.lua
@@ -0,0 +1,21 @@
+return {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    dependencies = {
+        {'neovim/nvim-lspconfig'},
+        {'williamboman/mason.nvim'},
+        {'williamboman/mason-lspconfig.nvim'},
+        {'hrsh7th/nvim-cmp'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'L3MON4D3/LuaSnip'},
+    },
+    config = function ()
+        local lsp = require('lsp-zero')
+
+        lsp.on_attach(function(client, bufnr)
+            lsp.default_keymaps({buffer = bufnr})
+        end)
+
+        lsp.setup()
+    end
+}

e0e0eee55be731f31916b9ab35b394bba41e246d
sant raj <santraj611@gmail.com>
2025-11-12 20:28:18 +0530

    lsp buffer formatting on save fixed for all lsps

--- lua/private/plugins/lspconfig.lua
+++ lua/private/plugins/lspconfig.lua
@@ -17,19 +17,15 @@ return {
     local servers = { 'lua_ls', 'ols', 'zls', 'basedpyright', 'cssls', 'html', 'clangd' }
 
     vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('my.lsp', {}),
       callback = function(args)
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        if client:supports_method('textDocument/completion') then
-          vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-        end
-        if not client:supports_method('textDocument/willSaveWaitUntil')
-            and client:supports_method('textDocument/formatting') then
+        if not client then return end
+
+        if client:supports_method('textDocument/formatting') then
           vim.api.nvim_create_autocmd('BufWritePre', {
-            group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
             buffer = args.buf,
             callback = function()
-              vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+              vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
             end,
           })
         end

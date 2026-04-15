9ed6bd7a0e1a2a7d10288e0664783329d3fc1ed4
sant raj <santraj611@gmail.com>
2026-04-15 21:53:14 +0530

    removed multiple colorschemes and switched to rose pine

--- init.lua
+++ init.lua
@@ -15,34 +15,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- setting up defualt colorscheme
----@param fallback? string
-_G.get_colorscheme = function(fallback)
-  if not vim.g.COLORS_NAME then
-    vim.cmd.rshada()
-  end
-  if not vim.g.COLORS_NAME or vim.g.COLORS_NAME == '' then
-    return fallback or 'default'
-  end
-  return vim.g.COLORS_NAME
-end
-
----@param colorscheme? string
-_G.save_colorscheme = function(colorscheme)
-  colorscheme = colorscheme or vim.g.colors_name
-  if get_colorscheme() == colorscheme then
-    return
-  end
-  vim.g.COLORS_NAME = colorscheme
-  vim.cmd.wshada()
-end
-
 require("lazy").setup({
   spec = {
     { import = 'private.plugins' },
-  },
-  install = {
-    -- Set the colorscheme for the `:Lazy` UI
-    colorscheme = { get_colorscheme 'default' },
-  },
+  }
 })
